7ffa06e17beae5621724294bc0987ba39343922f
Shagufta <shagufta@ironqabinet.com>
2015-09-10 23:18:44 +0530

    initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+
+export default Ember.ArrayController.extend({
+    isCompany: true,
+ firstName:"Zubair",
+    lastName: "Ahmed",
+    courses: function(){
+        return courses
+}.property()
+});
+    
+var courses=[
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
---
+++ app/routes/geonames.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+
+export default Ember.Route.extend({
+    
+   model: function(params) {
+    return Ember.$.getJSON("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo");
+  }
+});
